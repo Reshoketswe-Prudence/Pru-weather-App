75d530f54221e9977bcc8948fffe177dfd895eec
Reshoketswe Prudence Tlou <prudencetlou98@gmail.com>
2024-04-28 14:27:55 +0200

    Added API to my weather app

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  let temperature = Math.round(response.data.temperature.current);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+function cityChange(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-search");
+  let city = searchInput.value;
+
+  let apiKey = "aft4b837a65cf0aa680cbef4dfdo8c79";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
 function addingDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
